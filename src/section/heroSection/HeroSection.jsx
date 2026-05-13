d78d3594c96ed6ce87092222284a9d0b83cf0724
Ler98 <lisa.eriksson98@hotmail.com>
2026-05-13 18:03:38 +0200

    design på hero klar

--- src/section/heroSection/HeroSection.jsx
+++ src/section/heroSection/HeroSection.jsx
@@ -1,17 +1,21 @@
 import "../../section/heroSection/heroSection.css";
-import test from "../../assets/bilder/test.jpg";
+import jag from "../../assets/bilder/jag.jpg";
+import github from "../../assets/iconer/github.svg";
+import linkedin from "../../assets/iconer/linkedin.svg";
 
 function HeroSection() {
   return (
     <section className="hero">
       {/* <section className="hero__top"> */}
       <section className="hero__title-section">
-        <h1 className="hero__title">Lisa Eriksson</h1>
-        <h2 className="hero__subtitle">Webbutvecklare & GrafiskDesigner</h2>
+        <section className="hero__title-center">
+          <h1 className="hero__title">Lisa Eriksson</h1>
+          <h2 className="hero__subtitle">Webbutvecklare & GrafiskDesigner</h2>
+        </section>
       </section>
       <section className="img__profil-sectiion">
         <section className="img__border"></section>
-        <img className="img__profil" src={test} alt="profilbild" />
+        <img className="img__profil" src={jag} alt="profilbild" />
       </section>
       {/* </section> */}
       <section className="hero__about-section">
@@ -21,15 +25,25 @@ function HeroSection() {
           väckt ett nytt intresse för hur design och användarupplevelsen kan
           byggas med kod. Jag tycker tillgänglighet är viktigt och vill
           förbättra förutsättningar för alla så upplevelsen blir den samma.
+          <br />
+          <br />
+          Jag brinner för att skapa design i Figma, illustrator och indesign.
+          Det intresserar mig att se något växa fram från idé till slut. Css har
+          väckt ett nytt intresse för hur design och användarupplevelsen kan
+          byggas med kod. Jag tycker tillgänglighet är viktigt och vill
+          förbättra förutsättningar för alla så upplevelsen blir den samma.
         </p>
       </section>
-      <section className="hero__link-section">
-        <a className="hero__link" href="">
+      <section className="hero__nav-section">
+        <a className="hero__nav" href="">
           Kod-projekt
         </a>
-        <a className="hero__link" href="">
+        <a className="hero__nav" href="">
           Design-projekt
         </a>
+        {/* <section className="nav__ikon-section"> */}
+        <img className="nav__ikon" src={github} alt="link till github" />
+        <img className="nav__ikon" src={linkedin} alt="link till linkedin" />
       </section>
     </section>
   );
